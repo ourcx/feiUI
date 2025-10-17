fe61e300a368af5400e07c2d1039abcf27c0b355
小海 <3277975910@qq.com>
2025-10-17 20:38:23 +0800

    feat: 配置相关的husky

--- src/App.vue
+++ src/App.vue
@@ -558,6 +558,9 @@ const handleLayoutComplete = (words: any[]) => {
   <br>
     <Tinymce/>
     <Math/>
+    <Math format="i\hbar\frac{\partial}{\partial t}\Psi(\mathbf{r},t) = \left[ -\frac{\hbar^2}{2m}\nabla^2 + V(\mathbf{r},t) \right]\Psi(\mathbf{r},t)" />
+    <Math format="\frac{1}{2}\sum_{i=1}^n x_i^2" />
+
   <Footer> </Footer>
   </main>
 
